cda47c7de43ee9b90cf10d24870cded141428b55
Maulik <maulik@zluck.com>
2021-09-09 16:39:44 +0530

    build created

--- src/widgets/WalletModal/useWalletModal.tsx
+++ src/widgets/WalletModal/useWalletModal.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import { useModal } from "../Modal";
 import ConnectModal from "./ConnectModal";
 import AccountModal from "./AccountModal";
+import BlankConnectModal from './BlankConnectModal';
 import { Login } from "./types";
 
 interface ReturnType {
   onPresentConnectModal: () => void;
+  onPresentConnectModal1: () => void;
   onPresentAccountModal: () => void;
 }
 
 const useWalletModal = (login: Login, logout: () => void, account?: string): ReturnType => {
   const [onPresentConnectModal] = useModal(<ConnectModal login={login} />);
+  const [onPresentConnectModal1] = useModal(<BlankConnectModal login={login} />);
   const [onPresentAccountModal] = useModal(<AccountModal account={account || ""} logout={logout} />);
-  return { onPresentConnectModal, onPresentAccountModal };
+  return { onPresentConnectModal, onPresentAccountModal, onPresentConnectModal1 };
 };
 
 export default useWalletModal;
