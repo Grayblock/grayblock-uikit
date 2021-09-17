d2a0df76e3545a87d7d6510287b740bc7337ed27
Maulik <maulik@zluck.com>
2021-09-17 17:05:12 +0530

    card color changes

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -50,7 +50,7 @@ export const darkColors: Colors = {
   textDisabled: "#666171",
   textSubtle: "#A28BD4",
   borderColor: "#524B63",
-  card: "#27262c",
+  card: "#383E42",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
   },
